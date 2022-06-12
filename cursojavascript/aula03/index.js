49970e5d587857c0ab01fd48bc0ccd7a4aae0261
Fe1ker <93716685+Fe1ker@users.noreply.github.com>
2022-06-12 13:09:10 -0300

    terceira aula

---
+++ cursojavascript/aula03/index.js
@@ -0,0 +1,13 @@
+const nome = 'Matheus';
+const sobreNome = 'santos';
+let idade = 25;
+let peso = 84;
+let alturaEmM = 1.7;
+let IMC;
+const anoNascimento = 1996;
+
+IMC = peso/(alturaEmM * alturaEmM) 
+
+console.log(nome,sobreNome,'Nasceu em ',anoNascimento,'tem',idade,'anos, pesa',peso,'Kg, tem',alturaEmM,'de altura e seu IMC é de ',IMC);
+
+console.log(`${nome},${sobreNome} Nasceu em  ${anoNascimento} tem idade anos pesa ${peso} Kg, tem ${alturaEmM} de altura e seu IMC é de  ${IMC}`);
