8c60be8519bc0899dea9c66397ec9c9c0cbe2ed5
Fe1ker <93716685+Fe1ker@users.noreply.github.com>
2022-06-12 13:37:57 -0300

    testestando inserir aula04

--- cursojavascript/aula03/index.js
+++ cursojavascript/aula03/index.js
@@ -6,7 +6,7 @@ let alturaEmM = 1.7;
 let IMC;
 const anoNascimento = 1996;
 
-IMC = peso/(alturaEmM * alturaEmM) 
+IMC = peso/(alturaEmM * alturaEmM);
 
 console.log(nome,sobreNome,'Nasceu em ',anoNascimento,'tem',idade,'anos, pesa',peso,'Kg, tem',alturaEmM,'de altura e seu IMC é de ',IMC);
 
