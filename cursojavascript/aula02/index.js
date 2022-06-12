b08053ef28ecb255117e006b1c0a1741971f1547
Fe1ker <93716685+Fe1ker@users.noreply.github.com>
2022-06-12 13:06:00 -0300

    conteudo aula 2

---
+++ cursojavascript/aula02/index.js
@@ -0,0 +1,22 @@
+//let nome = 'duardão';
+
+
+
+
+//console.log(nome," nasceu em 1984.");
+//console.log("Em 2000",nome,"conheceu maria.");
+//console.log(nome," casou-se com maria em 2012.");
+//console.log("maria teve um filho com ",nome,"em 2015.");
+//console.log("o filho de ",nome," se chama eduardo.");
+
+//let nome_cliente = 'Matheus';
+//let nomeCliente = 'santos';
+//console.log(nomecliente);
+//console.log(nomeCliente);
+
+
+let primeiroNumero = 5;
+let segundoNumero = 10;
+resultado = primeiroNumero * segundoNumero
+resultado += 20
+console.log(resultado);
