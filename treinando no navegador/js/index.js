89f68559a9bc42713bfb40bc4476d297f7b1878f
Fe1ker <93716685+Fe1ker@users.noreply.github.com>
2022-06-13 22:04:11 -0300

    treinando no Navegador

---
+++ treinando no navegador/js/index.js
@@ -0,0 +1,25 @@
+alert('cara tu joga basquete pra krl se anima!');
+window
+window.confirm('sabia que tu joga basquete pra krl?');
+true
+window.prompt('digite o seu cpf meu bb :');
+'13220351646'
+
+const confirma = confirm('voce dejesa realmente apagar?'); // salvando a resposta da pergunta em uma variavelif
+if(confirma == True){
+    console.log('olha como ele vai aceitando..')
+}
+else{
+    console.log('achou que ele ia aceitar ? achou errado otario...')
+}
+
+
+
+let num1 = prompt('digite um numero:');
+num1 = Number(num1);
+let num2 = prompt('digite o segundo numero');
+num2 = Number(num2);
+const resultado = (num1+num2);
+alert('O resultado da soma foi :',resultado);
+
+// resolver isso ...
