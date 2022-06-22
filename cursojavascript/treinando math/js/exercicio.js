e468761f761ddcfbeb686d0c339ba2b551f5d634
Fe1ker <matheuscontato96@hotmail.com>
2022-06-22 12:24:59 -0300

    atualizando o exercicio

--- cursojavascript/treinando math/js/exercicio.js
+++ cursojavascript/treinando math/js/exercicio.js
@@ -3,4 +3,9 @@ const numerotitulo = document.getElementById("numero-titulo");
 const texto = document.getElementById("texto");
 
 numerotitulo.innerHTML = numero;
-texto.innerHTML = `<p> seu numero + 2 é ${numero + 2}</p><p> Raiz Quadrada : ${Math.sqrt(numero)}</p>`;
+texto.innerHTML += `<p> Raiz Quadrada : ${numero ** 0.5}</p>`;
+texto.innerHTML += `<p> número é inteiro? : ${Number.isInteger(numero)}</p>`;
+texto.innerHTML += `<p> é um NaN ?  ${Number.isNaN(numero)}</p>`;
+texto.innerHTML += `<p> arredondando para baixo fica: ${Math.floor(numero)}</p>`;
+texto.innerHTML += `<p> arredondado para cima é : ${Math.ceil(numero)}</p>`;
+texto.innerHTML += `<p>e com duas casas decimais é : ${numero.toFixed(2)}</p>`;
