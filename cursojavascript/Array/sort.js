a38ee91701d431304b158c22c764131aeb7d799e
Fe1ker <matheuscontato96@hotmail.com>
2022-06-27 10:27:16 -0300

    Atualisando Array

---
+++ cursojavascript/Array/sort.js
@@ -0,0 +1,9 @@
+const vendedores = ["Maria","Ana","Ronyevon", "Raquel","Madruga"];
+const sorteio = vendedores [Math.floor(Math.random() * vendedores.length)];
+console.log (sorteio);
+
+// push serve para adicionar um elemento no fim do vetor
+//unshift serve para adicionar no inicio do vetor
+//pop exclui o  ultimo elemento
+// shift remove elementos no incio do vetor
+// slice mostrar todos os elementos que estao em um determinado intervalo
